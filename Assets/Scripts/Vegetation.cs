f0462b24043695293bbf9c002f5657eab5458301
gorevojd <gorevojd27@gmail.com>
2020-10-27 16:51:51 +0300

    First episode

---
+++ Assets/Scripts/Vegetation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vegetation : MonoBehaviour
+{
+    public float minGrowTime = 0.8f;
+    public float maxGrowTime = 1.2f;
+
+    float growTime;
+    float lifeTime;
+
+    void Start()
+    {
+        transform.localScale = Vector3.zero;
+
+        lifeTime = 0f;
+
+        growTime = Random.Range(minGrowTime, maxGrowTime);
+    }
+
+    void Update()
+    {
+        float scale = lifeTime / growTime;
+        scale = Mathf.Clamp01(scale);
+        scale = Mathf.SmoothStep(0, 1, scale);
+
+        transform.localScale = Vector3.one * scale;
+
+        lifeTime += Time.deltaTime;
+    }
+}
